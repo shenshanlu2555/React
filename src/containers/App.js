f5b446ea2f35e6178dc06ce07b00235c25100d21
wgc <l171768979@163.com>
2018-12-20 14:29:51 +0800

    初始化

---
+++ src/containers/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import 'common/css/style.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="wrap">
+       
+      </div>
+    );
+  }
+}
+
+export default App;
